87c829b4164b1e17a619f5645071c97e9d934970
moshe <hyim695064@gmail.com>
2026-06-28 16:38:01 +0300

    part1s5

--- toolkit.py
+++ toolkit.py
@@ -2,11 +2,42 @@
 #     print("hello", name)
 # greet("agent x")
 #2
-def add(a, b):
-    print(a +b)
-add(3, 4)
+# def add(a, b):
+#     print(a +b)
+# add(3, 4)
 #3
-def square(n):
-    print(n ** 2)
-square(5)
-square(12)
+# def square(n):
+#     print(n ** 2)
+# square(5)
+# square(12)
+#4
+# def greet_with_title(name, title="agent"):
+#     print("hello", title, name)
+# greet_with_title("x")
+# greet_with_title("x", title = "person")
+#5
+def describe(name, level, active):
+     print("name", name)
+     print("level", level)
+     print("active", active)
+describe(name = "agent x", level = 5, active =True)
+describe(level = 5, active = True, name = "agent x")
+# name = "bob"
+# def change_name():
+#     name ="alice"
+# change_name()
+# print(name)
+# x = 10
+# def show():
+#     x = 5
+#     print(x)
+# show()
+# print(x)
+# x = 3
+# def chnge():
+#     global x
+#     x = 8
+# chnge()
+# print(x)
+
+
